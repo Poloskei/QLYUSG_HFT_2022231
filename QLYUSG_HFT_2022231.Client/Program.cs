ecc4d60d1d357b255ff7b4d9389bf23e6dd394e7
Sajátgép <Sajtgp@DESKTOP-AHTU16O>
2022-10-04 16:11:42 +0200

    Add project files.

---
+++ QLYUSG_HFT_2022231.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QLYUSG_HFT_2022231.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
